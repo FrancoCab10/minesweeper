0a38fdb36675c441062d6090563251b39795293f
Franco <gomez_f@outlook.com>
2019-11-19 19:55:52 -0300

    created grid rows and cells

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CellComponent } from '../cell/cell.component';
 
 @Component({
   selector: 'app-game',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameComponent implements OnInit {
 
+  private grid: CellComponent[][] = [];
+
   constructor() { }
 
   ngOnInit() {
+
+    // Initializing grid
+    let cellId = 1;
+    for (let row = 0; row < 10; row++) {
+      
+      const aux = [];
+      for (let col = 0; col < 10; col++) {
+        aux[col] = cellId;
+        cellId++;
+      }
+      
+      this.grid[row] = aux;
+    }
+
   }
 
 }
