c3fc027f5cae979b649662617a6f84e7aa3635f9
Franco <gomez_f@outlook.com>
2019-11-19 19:28:00 -0300

    dividing game in components and creating basic layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { GameComponent } from './components/game/game.component';
+import { TimerComponent } from './components/timer/timer.component';
+import { CellComponent } from './components/cell/cell.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GameComponent
+    GameComponent,
+    TimerComponent,
+    CellComponent
   ],
   imports: [
     BrowserModule
